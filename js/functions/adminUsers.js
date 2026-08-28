fa624f3704d7859bd42a5c353a5ebe0432f1894a
Lio <leonelhernandezg.123@gmail.com>
2026-08-27 22:03:38 -0600

    se agregaron los archivos del test

---
+++ js/functions/adminUsers.js
@@ -0,0 +1,53 @@
+console.log("adminUSers.js");
+$(document).ready(function () {
+  $(document).on("click", ".btnSaveUser", function () {
+    var name = $("#name").val();
+    var lastname = $("#lastname").val();
+    var address = $("#address").val();
+    var mail = $("#mail").val();
+
+    $.ajax({
+      url: "php/controllers/userController.php",
+      method: "POST",
+      data: {
+        mod: "saveUser",
+        name: name,
+        lastname: lastname,
+        address: address,
+        mail: mail,
+      },
+    })
+      .done(function (data) {
+        var data = JSON.parse(data);
+        console.log(data);
+
+        if (data.response == true) {
+          location.reload();
+        } else {
+          Swal.fire({
+            title: "Error",
+            text: "No se pudo registrar el usuario",
+            icon: "danger",
+          });
+        }
+
+        //--- --- ---//
+        //--- --- ---//
+      })
+      .fail(function (message) {
+        VanillaToasts.create({
+          title: "Error",
+          text: "Ocurrió un error, intentelo nuevamente",
+          type: "error",
+          timeout: 1200,
+          positionClass: "topRight",
+        });
+      });
+  });
+  $(document).on("click", ".editUser", function () {
+    console.log("editUser function called");
+  });
+  $(document).on("click", ".deleteUser", function () {
+    console.log("deleteUser function called");
+  });
+});

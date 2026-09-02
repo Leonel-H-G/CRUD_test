d56d844841845c09ed0cf3499b5950f3132e392d
Lio <leonelhernandezg.123@gmail.com>
2026-09-01 22:26:06 -0600

    cambios en usercontroller, adminjs, index php

---
+++ php/controllers/userController.php
@@ -0,0 +1,58 @@
+<?php
+include_once dirname(__DIR__ . '', 1) . "/models/petitions.php";
+
+session_start();
+date_default_timezone_set('America/Mexico_City');
+
+if (!empty($_POST['mod'])) {
+    $function = $_POST['mod'];
+    $function();
+}
+
+
+function saveUser()
+{
+    
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $address = $_POST['address'];
+    $mail = $_POST['mail'];
+
+    $queries = new Queries;
+
+    $stmt = "INSERT INTO asteleco_test.users (
+        id_users_status,
+        name,
+        lastname,
+        addres,
+        mail) 
+        VALUES(
+            1,
+            '$name',
+            '$lastname',
+            '$address',
+            '$mail'
+        )";
+
+    $insertData = $queries->InsertData($stmt);
+    if (!empty($insertData)) {
+        $last_id = $insertData['last_id'];
+
+        //--- --- ---//
+        $data = array(
+            'response' => true,
+            'idUser'                => $last_id,
+            'message'  => 'registrado con exito'
+        );
+        //--- --- ---//
+    } else {
+        //--- --- ---//
+        $data = array(
+            'response' => false,
+            'message'                => 'error no se registra'
+        );
+        //--- --- ---//
+    }
+
+    echo json_encode($data);
+}
